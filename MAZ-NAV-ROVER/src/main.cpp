6b32dfb4741301ca809d97879ad03f3058287e11
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-11-19 01:14:02 +0500

    defined MACROS in seperate header file

--- MAZ-NAV-ROVER/src/main.cpp
+++ MAZ-NAV-ROVER/src/main.cpp
@@ -3,25 +3,10 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
-#define MOTOR_STEPS 200
-#define RPM 30
-#define MICROSTEPS 1
-
-#define DIR 12
-#define STEP 14
-#define DIR1 27
-#define STEP1 26
-#define WHITELED 25
-#define RIGHTSENS 33
-#define FRONTSENS 32
-#define LEFTSENS 35
-#define SCL 22
-#define SDA 21
-#define SERVOPWM 13
+#include "PinDefinitions.h"
 
 AccelStepper stepper1(AccelStepper::DRIVER, STEP, DIR);
 AccelStepper stepper2(AccelStepper::DRIVER, STEP1, DIR1);
