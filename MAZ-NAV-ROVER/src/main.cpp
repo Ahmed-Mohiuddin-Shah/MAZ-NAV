30d956a6683e910d760dcb83afabc163bdfff3bf
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2023-11-19 17:44:10 +0500

    restructure

--- MAZ-NAV-ROVER/src/main.cpp
+++ MAZ-NAV-ROVER/src/main.cpp
@@ -1,23 +1,5 @@
-// Basic Library Include for Arduino Framework
-#include <Arduino.h>
-
-// Library Includes for Sensors and I2C
-// TCS34725 Color Sensor
-// MPU 6050 Gyroscope and Accelerometer
-#include "Adafruit_TCS34725.h"
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
-
-// Library Includes for Stepper Drivers
-#include <AccelStepper.h>
-#include <MultiStepper.h>
-
-// Bluetooth BLE Library Includes for ESP32
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+// Include all of Includes for project
+#include <Includes.h>
 
 // Headers files
 #include "Definitions.h"
