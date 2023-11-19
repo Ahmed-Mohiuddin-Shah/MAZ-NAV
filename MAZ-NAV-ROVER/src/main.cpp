ae6d5e5419c480ab4738737b8168304b4e511123
Hamza <muhammadhamzam1486@gmail.com>
2023-11-20 00:14:02 +0500

    Update folder structure again. Remove flutter things. Update Remove BLE and setup classic bluetooth.

--- MAZ-NAV-ROVER/src/main.cpp
+++ MAZ-NAV-ROVER/src/main.cpp
@@ -1,114 +1,87 @@
-// Include all of Includes for project
-#include <Includes.h>
+// Basic Library Include for Arduino Framework
+#include <Arduino.h>
+
+// Library Includes for Sensors and I2C
+// TCS34725 Color Sensor
+// MPU 6050 Gyroscope and Accelerometer
+#include "Adafruit_TCS34725.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+// Library Includes for Stepper Drivers
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
+#include "BluetoothSerial.h"
 
 // Misc.
 #include <string>
 
 // Headers files
-#include <Definitions.h>
+#include <macros.h>
 
 // Sensor Global Variables
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
-Adafruit_MPU6050 mpu;
+Adafruit_TCS34725 tcsColorSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+Adafruit_MPU6050 mpuGyroModule;
 
 // Stepper Drivers Global Variables
-AccelStepper stepper1(AccelStepper::DRIVER, STEP, DIR);
-AccelStepper stepper2(AccelStepper::DRIVER, STEP1, DIR1);
+AccelStepper stepper1(AccelStepper::DRIVER, STEP, MOTOR_DIR_1);
+AccelStepper stepper2(AccelStepper::DRIVER, STEP1, MOTOR_DIR_2);
 MultiStepper steppers;
 
-// BLE ESP32 Global Variables
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-
 long positions[2]; // Array of desired stepper positions
-
-// Custom Server Call Backs
-class MyServerCallbacks : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    Serial.println("Device connected");
-  }
-
-  void onDisconnect(BLEServer *pServer)
-  {
-    Serial.println("Device disconnected");
-  }
-};
-
-// Custom Characteristic Call Backs
-class MyCharacteristicCallbacks : public BLECharacteristicCallbacks
-{
-  void onWrite(BLECharacteristic *pCharacteristic)
-  {
-    std::string value = pCharacteristic->getValue();
-
-  }
-
-  void onRead(BLECharacteristic *pCharacteristic)
-  {
-    pCharacteristic->setValue("69");
-  }
-};
+BluetoothSerial SerialBT;
 
 // Setup Code
 void setup()
 {
-
-  // Create the BLE Device
-  BLEDevice::init("MAZ-NAV-ROVER");
-
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  // Create the BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE);
-
-  // Add a descriptor for the characteristic
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  // Start the service
-  pService->start();
-
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->start();
-
+  Serial.begin(9600);
+  SerialBT.begin("MAZ-NAV"); // Bluetooth device name
   // Configure each stepper
-  stepper1.setMaxSpeed(200);
-  stepper1.setAcceleration(10);
+  stepper1.setMaxSpeed(MOTOR_MAX_SPEED);
+  stepper1.setAcceleration(MOTOR_ACCELERATION);
 
-  stepper2.setMaxSpeed(200);
-  stepper2.setAcceleration(10);
-  
+  stepper2.setMaxSpeed(MOTOR_MAX_SPEED);
+  stepper2.setAcceleration(MOTOR_ACCELERATION);
 
   // Assign to MultiStepper to manage
   steppers.addStepper(stepper1);
   steppers.addStepper(stepper2);
 
   // Configuring Inputs and Outputs
-  pinMode(SERVOPWM, OUTPUT);      // Servo pin OUT
-  pinMode(WHITELED, OUTPUT);      // RGB Sensor LED OUT
-  pinMode(RIGHTSENS, INPUT);      // Right Line Sensor Input
-  pinMode(FRONTSENS, INPUT);      // Front Line Sensor Input
-  pinMode(LEFTSENS, INPUT);       // Left Line Sensor Input
+  pinMode(SERVOPWM, OUTPUT); // Servo pin OUT
+  pinMode(WHITELED, OUTPUT); // RGB Sensor LED OUT
+  pinMode(RIGHTSENS, INPUT); // Right Line Sensor Input
+  pinMode(FRONTSENS, INPUT); // Front Line Sensor Input
+  pinMode(LEFTSENS, INPUT);  // Left Line Sensor Input
 
   // Setting up the MPU6050
-  mpu.begin(0x68);
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  mpuGyroModule.begin(0x68);
+  mpuGyroModule.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpuGyroModule.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpuGyroModule.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
 void loop()
 {
+  String inputFromOtherSide;
+  if (SerialBT.available())
+  {
+    inputFromOtherSide = SerialBT.readString();
+    SerialBT.println("You had entered: ");
+    SerialBT.println(inputFromOtherSide);
+  }
 
+  // positions[0] = 200;
+  // positions[1] = 200;
+  // steppers.moveTo(positions);
+  // steppers.runSpeedToPosition();
+
+  // delay(500);
+
+  // positions[0] = -200;
+  // positions[1] = -200;
+  // steppers.moveTo(positions);
+  // steppers.runSpeedToPosition();
 }
